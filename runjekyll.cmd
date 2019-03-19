260d7e0030dcc753736ec1b269578dfea9b513c3
flensted <nfj@flensted-jensen.com>
2019-03-19 13:51:26 +0100

    Switched to 32 bit (x64)

--- runjekyll.cmd
+++ runjekyll.cmd
@@ -2,7 +2,7 @@
 
 REM Put Ruby in Path
 REM You can also use %TEMP% but it is cleared on site restart. Tools is persistent.
-SET PATH=%PATH%;D:\home\site\deployments\tools\r\rubyinstaller-2.5.3-1-x86\bin
+SET PATH=%PATH%;D:\home\site\deployments\tools\r\rubyinstaller-2.5.3-1-x64\bin
 
 REM I am in the repository folder
 pushd D:\home\site\deployments
@@ -10,13 +10,13 @@ if not exist tools md tools
 cd tools 
 if not exist r md r
 cd r 
-if exist rubyinstaller-2.5.3-1-x86 goto end
+if exist rubyinstaller-2.5.3-1-x64 goto end
 
 echo No Ruby, need to get it!
 
 REM Get Ruby and Rails
-REM 32bit
-curl -o ruby253.zip -L https://github.com/oneclick/rubyinstaller2/releases/download/rubyinstaller-2.5.3-1/rubyinstaller-2.5.3-1-x86.7z?direct
+REM 64 bit
+curl -o ruby253.zip -L https://github.com/oneclick/rubyinstaller2/releases/download/rubyinstaller-2.5.3-1/rubyinstaller-2.5.3-1-x64.7z?direct
 REM Azure puts 7zip here!
 echo START Unzipping Ruby
 SetLocal DisableDelayedExpansion & d:\7zip\7za x -xr!*.ri -y ruby253.zip > rubyout
